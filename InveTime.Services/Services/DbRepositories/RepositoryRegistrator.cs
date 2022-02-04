1afa3989b16da524427f7c903501a2c75105bbdd
Артём Кондратьев <exitway12@gmail.com>
2022-02-04 18:03:00 +0300

    IT-RealizeRepositoryPattern: Add RepositoryRegistrator container of repository services

---
+++ InveTime.Services/Services/DbRepositories/RepositoryRegistrator.cs
@@ -0,0 +1,21 @@
+﻿using InveTime.DataEntities.Entities;
+using InveTime.Interfaces.Base.Repository;
+using InveTime.Services.Services.DbRepositories.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InveTime.Services.Services.DbRepositories
+{
+    public static class RepositoryRegistrator
+    {
+        public static IServiceCollection AddDbRepositories(this IServiceCollection services) => services
+            .AddTransient<IRepository<Category>, DbRepository<Category>>()
+            .AddTransient<IRepository<CategorySearchWord>, DbRepository<CategorySearchWord>>()
+            .AddTransient<IRepository<Employee>, DbRepository<Employee>>()
+            .AddTransient<IRepository<Inventory>, DbRepository<Inventory>>()
+            .AddTransient<IRepository<Market>, DbRepository<Market>>()
+            .AddTransient<IRepository<Position>, DbRepository<Position>>()
+            .AddTransient<IRepository<Product>, DbRepository<Product>>()
+            .AddTransient<IRepository<ProductInvented>, DbRepository<ProductInvented>>()
+            ;
+    }
+}
