9cd084aea78316bb1a963b448b73a25fa8d783e7
Артём Кондратьев <exitway12@gmail.com>
2022-02-08 02:31:36 +0300

    IT-RealizeHTTPClientWebRepository: Created WebRepository class

---
+++ InveTime.WebApi.Clients/WebRepository/WebRepository.cs
@@ -0,0 +1,77 @@
+﻿using InveTime.Interfaces.Base.Entity;
+using InveTime.Interfaces.Base.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InveTime.WebApi.Clients.WebRepository
+{
+    public class WebRepository<T> : IRepository<T> where T : IEntity, new()
+    {
+        //TODO: Realize Interface!!!
+        public bool AutoSaveChanges { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task<T> AddAsync(T item, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistAsync(T item, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistIdAsync(int id, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAfterSkipAsync(int Skip, int Count, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCountAsync(CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IPage<T>> GetPageAsync(int PageIndex, int PageSize, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RemoveAsync(T item, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> RemoveByIdAsync(int id, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T item, CancellationToken Cancel = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
