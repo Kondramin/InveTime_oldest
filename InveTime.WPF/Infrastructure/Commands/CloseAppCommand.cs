5dcb10ccb5d588203c7e7b6ad0d7b3d45df981d5
Артём Кондратьев <exitway12@gmail.com>
2022-02-07 18:11:08 +0300

    IT-RealizeWPFApp: Added global commands "CloseApp" & "DialogResult"

---
+++ InveTime.WPF/Infrastructure/Commands/CloseAppCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace InveTime.WPF.Infrastructure.Commands
+{
+    /// <summary>Command, what closing all Application</summary>
+    class CloseAppCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
