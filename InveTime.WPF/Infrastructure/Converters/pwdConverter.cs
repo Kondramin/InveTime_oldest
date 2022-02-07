4e0966ac25fcbcf2b4d7505d0c81601708a0d349
Артём Кондратьев <exitway12@gmail.com>
2022-02-07 18:11:49 +0300

    IT-RealizeWPFApp: Added password box converter to realize multi binding in future

---
+++ InveTime.WPF/Infrastructure/Converters/pwdConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace InveTime.WPF.Infrastructure.Converters
+{
+    internal class pwdConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            PasswordBox[] pwdBoxes = new PasswordBox[3];
+            pwdBoxes[0] = values[0] as PasswordBox;
+            pwdBoxes[1] = values[1] as PasswordBox;
+            pwdBoxes[2] = values[2] as PasswordBox;
+            return pwdBoxes;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
