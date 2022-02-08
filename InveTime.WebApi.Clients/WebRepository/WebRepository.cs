86e2f2c357d5680c76c76e61ed5cd959c9d6e95a
Артём Кондратьев <exitway12@gmail.com>
2022-02-08 10:12:56 +0300

    IT-RealizeHttpClientWebRepository: Preparation to work

--- InveTime.WebApi.Clients/WebRepository/WebRepository.cs
+++ InveTime.WebApi.Clients/WebRepository/WebRepository.cs
@@ -2,8 +2,7 @@
 using InveTime.Interfaces.Base.Repository;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,66 +10,82 @@ namespace InveTime.WebApi.Clients.WebRepository
 {
     public class WebRepository<T> : IRepository<T> where T : IEntity, new()
     {
-        //TODO: Realize Interface!!!
-        public bool AutoSaveChanges { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private readonly HttpClient _HttpClient;
+
+        public WebRepository(HttpClient httpClient) => _HttpClient = httpClient;
+
+        
+        public bool AutoSaveChanges { get; set; } = true;
 
         public Task<T> AddAsync(T item, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<bool> ExistAsync(T item, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<bool> ExistIdAsync(int id, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<IEnumerable<T>> GetAfterSkipAsync(int Skip, int Count, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<IEnumerable<T>> GetAllAsync(CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<T> GetByIdAsync(int id, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<int> GetCountAsync(CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<IPage<T>> GetPageAsync(int PageIndex, int PageSize, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<T> RemoveAsync(T item, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<T> RemoveByIdAsync(int id, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<int> SaveChangesAsync(CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
 
         public Task<T> UpdateAsync(T item, CancellationToken Cancel = default)
         {
+            //TODO: Realize method!!!
             throw new NotImplementedException();
         }
     }
