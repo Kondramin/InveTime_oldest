0701d02f72d31c0ed075847f01f94a4a76cd61ce
Артём Кондратьев <exitway12@gmail.com>
2022-02-14 17:54:41 +0300

    IT-RealizeWebApi: Added ModelControllers

---
+++ InveTime.WebApi/Controllers/EmployeeModelController.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using InveTime.DataEntities.Entities;
+using InveTime.Domain.Domains;
+using InveTime.Interfaces.Base.Repository;
+using InveTime.WebApi.Controllers.Base;
+
+namespace InveTime.WebApi.Controllers
+{
+    public class EmployeeModelController : MappFromEntityToModelController<Employee, EmployeeModel>
+    {
+        public EmployeeModelController(IRepository<Employee> repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}
---
+++ InveTime.WebApi/Controllers/InventoryModelController.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using InveTime.DataEntities.Entities;
+using InveTime.Domain.Domains;
+using InveTime.Interfaces.Base.Repository;
+using InveTime.WebApi.Controllers.Base;
+
+namespace InveTime.WebApi.Controllers
+{
+    public class InventoryModelController : MappFromEntityToModelController<Inventory, InventoryModel>
+    {
+        public InventoryModelController(IRepository<Inventory> repository, IMapper mapper) : base(repository, mapper) { }
+    }
+}
